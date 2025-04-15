2c80def4af65d622b6f3a164091694eb04ac4758
Ricardo Torrellas <ricardo.torrejas@gmail.com>
2025-04-15 05:30:45 -0400

    cuarto_commit

--- bin/crear/crear_commit.py
+++ bin/crear/crear_commit.py
@@ -5,7 +5,17 @@ from rich.console import Console
 console = Console()
 
 def crear_commit(titulo,contenido):
-    comando_ejecutar = f"git add . & git commit -m {titulo} -m {contenido} & git push"
+    comando_ejecutar = f"git add . && git commit -m {titulo} -m {contenido} && git push"
+    ejecucion = run(
+        comando_ejecutar,
+        capture_output=True,
+        text=True,
+        shell=True
+    )
+    return ejecucion
+
+def crear_commit_no_contenido(titulo):
+    comando_ejecutar = f"git add . && git commit -m {titulo} && git push"
     ejecucion = run(
         comando_ejecutar,
         capture_output=True,
@@ -22,16 +32,25 @@ def __todo_correcto(code,resultado):
 
 def __error(code,error):
     if code > 0:
-        print(error)
+        console.print(error)
         console.print(f"❌",f"[red]se ah producido un error[/]")
         sys.exit(1)
 
 def main():
-    titulo = sys.argv[1]
-    contenido =  sys.argv[2]
-    resultado = crear_commit(titulo,contenido)
-    __todo_correcto(resultado.returncode,resultado)
-    __error(resultado.returncode,resultado.stderr)
+    titulo = sys.argv[1] if len(sys.argv) > 1 else None
+    contenido =  sys.argv[2] if len(sys.argv) > 2 else None
+    if titulo:
+        
+        if contenido == None:
+            resultado = crear_commit_no_contenido(titulo)
+            __todo_correcto(resultado.returncode,resultado)
+            __error(resultado.returncode,resultado.stderr)
+            
+        resultado = crear_commit(titulo,contenido)
+        __todo_correcto(resultado.returncode,resultado)
+        __error(resultado.returncode,resultado.stderr)
+    __error(1,f"debe mandarse aunque sea un argumento, el valor del primer argumento: [red]{titulo}[/]")
+    
 
 if __name__ == "__main__":
     main()
